0be196e46f98489bf8c958c9962b1fd6790a1a2f
Sherif Alaa <sherif.alaa55@gmail.com>
2024-10-13 21:56:42 +0300

    added env variables

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from vanna.chromadb import ChromaDB_VectorStore
 from vanna.flask import VannaFlaskApp
 from vanna.openai import OpenAI_Chat
@@ -7,10 +8,10 @@ class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
         ChromaDB_VectorStore.__init__(self, config=config)
         OpenAI_Chat.__init__(self, config=config)
 
-vn = MyVanna(config={'api_key': '', 'model': 'gpt-4'})
+vn = MyVanna(config={'api_key': os.getenv('API_KEY'), 'model': os.getenv('MODEL')})
 
 
-vn.connect_to_mysql(host='127.0.0.1', dbname='', user='', password='', port=3306)
+vn.connect_to_mysql(host=os.getenv('DB_HOST'), dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASS'), port=3306)
 
 app = VannaFlaskApp(vn)
 app.run()
